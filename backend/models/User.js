4dbd057df13f106dd9fc79bb1285b4ae5492d8ea
MaitriKhokhariya <141763064+MaitriKhokhariya@users.noreply.github.com>
2025-11-19 16:36:28 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,41 +1,4 @@
-// // models/User.js
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcrypt';
-// const UserSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   avatar: {
-//     type: String,
-//     required: false,
-//   },
-// });
-// UserSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// UserSchema.methods.comparePassword = async function (candidatePassword) {
-//   return bcrypt.compare(candidatePassword, this.password);
-// };
-// const User = mongoose.model('User', UserSchema);
-
-// export default User;
-
-
-import mongoose from "mongoose";
+ import mongoose from "mongoose";
 import bcrypt from 'bcrypt';
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
